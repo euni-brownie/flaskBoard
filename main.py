0174cf4a1c1a226835c7c86afd5d66f38bb3dca2
jeongeun.kim <jeongeun.kim@ptbwa.com>
2021-05-17 02:35:57 +0900

    #2 [flaskBoard] 1차 과제 CRUD 구현 - 게시판 만들기 CRUD 기능 구현 및 예외처리 완료 - 단, create, update 시 ^와 같은 특수문자는 escape 처리 되지 않는 문제 있음

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 import model.article_dao as article_dao
+import json
 application = Flask(__name__)  # WSGI 실행 환경에 맞춰서 app 변수명 변경
 application.jinja_env.trim_blocks = True
 application.config['JSON_AS_ASCII'] = False  # json한글 처리 설정
@@ -18,7 +19,6 @@ def _hellohtml():
 @application.route('/main')
 def _main():
     article_list = article_dao.get_list()
-    print(article_list)
     return render_template("main.html", article_list=article_list)
 
 
@@ -27,7 +27,40 @@ def _write():
     return render_template("write.html")
 
 
-@application.route('/method', methods=['GET', 'POST'])
+@application.route('/edit')
+def _edit():
+    article = article_dao.get_article(request)
+    return render_template("edit.html", article=article)
+
+
+@application.route('/article')
+def _article():
+    article = article_dao.get_article(request)
+    return render_template("article.html", article=article)
+
+
+@application.route('/create', methods=['POST'])
+def _create():
+    result = article_dao.create_article(request)
+    return redirect(url_for('_main'))
+
+
+@application.route('/update', methods=['POST'])
+def _update():
+    article_idx, is_successed = article_dao.update_article(request)
+    if (is_successed):
+        return redirect(url_for('_article', articleIdx=article_idx, updated=is_successed))
+    else:
+        return redirect(url_for('_edit', articleIdx=article_idx, updated=is_successed))
+
+
+@ application.route('/delete')
+def _delete():
+    result = article_dao.delete_article(request)
+    return redirect(url_for('_main'))
+
+
+@ application.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
         return "GET으로 전달"
--- model/article_dao.py
+++ model/article_dao.py
@@ -1,5 +1,10 @@
+from sqlalchemy import text
 import connection as dbconn
 import pandas as pd
+from flask import request
+import datetime
+# from html.parser import HTMLParser
+import html
 
 
 def get_list():
@@ -18,3 +23,89 @@ def get_list():
     article_list = df_result.to_dict('records')
     print(article_list)
     return article_list
+
+
+def get_article(request):
+    article_Idx = request.args.get('articleIdx')
+    engine = dbconn.mysql_engine()
+    with engine.connect() as conn:
+
+        query = f"""
+                    SELECT * FROM ARTICLE
+                    WHERE articleIdx = {article_Idx} ;
+                """
+
+        result = conn.execute(query)
+    df_result = pd.DataFrame(
+        result.fetchall(), columns=result.keys())
+    article = df_result.to_dict('records')
+
+    return article
+
+
+def create_article(request):
+    article = {
+        "article_title": request.form['article_title'],
+        # "article_title": html.escape(request.form['article_title']),
+        "article_author": request.form['article_author'],
+        "article_content": request.form['article_content'],
+        # "article_content": html.escape(request.form['article_content']),
+        "article_password": request.form['article_password'],
+    }
+    engine = dbconn.mysql_engine()
+    with engine.connect() as conn:
+
+        query = f"""
+                    INSERT INTO ARTICLE(userId,title,content,password)
+                    VALUES("{article['article_author']}", "{article['article_title']}", "{article['article_content']}","{article['article_password']}");
+                """
+
+        result = conn.execute(query)
+        isSuccessed = True if result.rowcount == 1 else False
+    return isSuccessed
+
+
+def delete_article(request):
+    article_Idx = request.args.get('articleIdx')
+    engine = dbconn.mysql_engine()
+    with engine.connect() as conn:
+
+        query = f"""
+                    DELETE FROM ARTICLE
+                    WHERE articleIdx = {article_Idx} ;
+                """
+
+        result = conn.execute(query)
+    isSuccessed = True if result.rowcount == 1 else False
+
+    return isSuccessed
+
+
+def update_article(request):
+    curdate = datetime.datetime.now()
+    curdate = curdate.strftime("%Y-%m-%d %H:%M")
+
+    article = {
+        "article_title": request.form['article_title'],
+        # "article_title": html.escape(request.form['article_title']),
+        "article_idx": request.form['article_idx'],
+        "article_author": request.form['article_author'],
+        "article_content": request.form['article_content'],
+        # "article_content": html.escape(request.form['article_content']),
+        "article_password": request.form['article_password'],
+    }
+
+    engine = dbconn.mysql_engine()
+    with engine.connect() as conn:
+
+        query = f"""
+                    UPDATE ARTICLE SET title = "{article['article_title']}", content = "{article['article_content']}", regDate="{curdate}"
+                    WHERE articleIdx = {article['article_idx']};
+                """
+        result = conn.execute(query)
+        # query = 'UPDATE ARTICLE SET title=:title, content=:content, regDate=:regDate WHERE articleIdx=:articleIdx'
+        # result = conn.execute(
+        #     query, title=article['article_title'], content=article['article_content'], regDate=curdate, articleIdx=article['article_idx'])
+        print("##rowcount : ", result.rowcount)
+    is_successed = True if result.rowcount == 1 else False
+    return article['article_idx'], is_successed
